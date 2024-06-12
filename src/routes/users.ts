cd4e02e257f757699a664f974ec39b0f7238763e
Iannaan <moshou5571228@gmail.com>
2024-06-13 07:41:24 +0800

    nodemon setting

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,7 +6,7 @@ const logger = getLogger('USER_ROUTE');
 /* GET users listing. */
 router.get('/', function (_req, res, _next) {
   logger.info('respond with a resource');
-  res.send('respond with a resource');
+  res.json({ status: "success", message: "Good to use,"})
 });
 
 export default router;
