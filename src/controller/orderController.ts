523756941410a6cc6b2586b1b9b9b3a93161adeb
Iannaan <moshou5571228@gmail.com>
2024-06-28 21:12:20 +0800

    implement  create  order  validatoe from  frontend data

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -1,7 +1,9 @@
 import { IOrderModel, OrderContent, PaymentProvider, PaymentWay } from "@/model/order";
 import { IProductModel } from "@/model/products";
 import { NextFunction, Request, Response } from "express";
+import { ValidationChain, body, validationResult } from "express-validator";
 import { Knex } from "knex";
+import { isEmpty } from "lodash";
 
 
 
@@ -17,7 +19,8 @@ interface CreateOrderRequestParams {
 }
 
 export interface IOrderController {
-    createdOrder(
+    createOrderValidator(): ValidationChain[],// 去下面看body 會要哪些參數 xxxxx:ValidationChain
+    createOrder(
         req: Request<any, any,  CreateOrderRequestParams, any>,
         res: Response,
         next: NextFunction
@@ -35,10 +38,24 @@ export class OrderController implements IOrderController {
     orderModel: IOrderModel;
     productModel: IProductModel;
 
+    // class 要補上Static 跟 product一樣 static createController
+    // 跟product 一樣 createController return new orderController 參數戴上
+    public static createController = ({
+        knexSql,
+        orderModel,
+        productModel
+    }:{
+        knexSql: Knex;
+        orderModel: IOrderModel;
+        productModel: IProductModel;
+        }) =>  {
+        return new OrderController({ knexSql,orderModel,productModel })
+    }
+
     // 要實作方法要先 constructor 如果前面有參數要處理就不會先寫constructor
     // 這個物件需要 transaction function => Knex  要從app當中調用要給他一個接口 (參數)
     // orderModel, productModel => 建立訂單 刷新商品
-    constructor( {knexSql, orderModel, productModel}:{
+    constructor({ knexSql, orderModel, productModel }:{
         knexSql: Knex;
         orderModel: IOrderModel;
         productModel: IProductModel;
@@ -48,12 +65,76 @@ export class OrderController implements IOrderController {
         this.orderModel = orderModel;
         this.productModel = productModel;
     }
+
+    // 驗證就是把資料抓進來不用輸入參數 用 return array []搜集多筆資料 寫厚道跑routes
+    public createOrderValidator = () =>{
+    
+        // 設定參數驗證在之前的方法要有這些內容 
+        const paymentProviderValidator = (value: any ) => {
+            return [PaymentProvider.ECPAY, PaymentProvider.Paypal].includes(value);
+        }
+        const paymentWayValidator = (value: any) => {
+            return [PaymentWay.CVS, PaymentWay.PAYPAL].includes(value);
+        }
+        // Content 可以any 或者是 OrderContent[] 使用後者好處 productId直接寫
+        const contentsValidator = (value: OrderContent[]) => {
+            // 理論上要有資料
+            if (isEmpty(value)) false;
+            // 抓內部資料就是用for loop
+            for (const product of value) {
+                if (
+                    [product.productId, product.amount, product.price].some(val => !val)
+                ) 
+                    return false;
+            }
+            return true;
+        };
+
+        return [
+            body('paymentProvider', "Invalid payment provider").custom(
+                paymentProviderValidator
+            ),
+            body('paymentPay', "Invalid payment pay").custom(
+                paymentWayValidator
+            ),
+            // contents 比較特別 有三個錯誤
+            body('contents', 'Invalid product provider content')
+            .isArray()
+            .custom(
+                contentsValidator
+            ),
+        ];
+    };
     // 要接IOrderController話吃路的大餅在這邊實作
     // 只要從前端拿到商品id 數量  payment_provider payment_way 其他從後端拿 這幾個為req
-    public createOder: IOrderController['createdOrder'] = (req, res, _next) => {
+    public createOrder: IOrderController['createOrder'] = (req, res, _next) => {
         // req 記得要寫成JS形狀 content期望是[ {product.id, product.amount ,product.price}]
         // 用鼠標指導req 如果還是any 要上去IOrderController補定義 自己定義參數名稱 i.e:CreateOrderRequestParameter() 並且到最上方寫interface(直接定義)
         let { paymentProvider, paymentWay, contents } = req.body
+        // 先打看看能不能有參數進到這個method 到前台要收到資料mounted 也就是要設計一個router 跑vue mounted
+        console.log(paymentProvider, paymentWay,contents);
+         // 測試用打api 通過就給他 res.json({'status': 'success'})
+
+        // 1.資料驗證 方法1 自己寫邏輯 / 方法2 線上套件 ===>其一 express validator 1.安裝後 先建立新方法 public method ...
+        // https://express-validator.github.io/docs/guides/getting-started => ValidationResult
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({errors: errors.array() });
+        }
+      
+        
+       
+   
+        // 2. 將資料入db => order
+
+        // 3. 金流
+
+    };
+
+    public updateAmount: IOrderController["updateAmount"] =(req, res, _next) => {
+        // TODO
     }
+    
 
  }
