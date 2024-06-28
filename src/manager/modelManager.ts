5553411096bdad1ef8abcd576c0046dd64250a6a
Iannaan <moshou5571228@gmail.com>
2024-06-28 21:08:54 +0800

    Add OrderModel to modelManager and update ModelContext

--- src/manager/modelManager.ts
+++ src/manager/modelManager.ts
@@ -1,3 +1,4 @@
+import { IOrderModel, OrderModel } from "@/model/order";
 import { IProductModel, ProductModel } from "@/model/products";
 import { Knex } from "knex";
 // 創建管理層 做model 管理 透過modelManager 加入app.ts 方式來啟用功能
@@ -5,11 +6,14 @@ import { Knex } from "knex";
 // IProductModel
 export interface ModelContext {
     productModel: IProductModel;
+    // 增加完IOrderModel 後下面要補
+    orderModel: IOrderModel;
 }
 
 export const  modelManager = ( { knexSql }: { knexSql: Knex }): ModelContext => {
     // return 各種model
     const productModel = ProductModel.createModel({ knexSql })
-    return { productModel }; 
+    const orderModel = OrderModel.createModel({ knexSql })
+    return { productModel, orderModel }; 
 
 }
